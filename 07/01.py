e49ee8d46a66f6a7c294f864053241aee0b92d26
Russ <72242656+Russ741@users.noreply.github.com>
2023-12-07 00:34:20 -0500

    Day 7 Part 1.

---
+++ 07/01.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+hand_map = {
+    'A': 14,
+    'K': 13,
+    'Q': 12,
+    'J': 11,
+    'T': 10,
+}
+hand_map.update({str(i):i for i in range(9, 1, -1)})
+print(hand_map)
+
+def hand_key(hand_and_bid):
+    hand = hand_and_bid[0]
+    key = 0
+    counts = defaultdict(int)
+    for card in hand:
+        key = key * 100 + hand_map[card]
+        counts[card] += 1
+    sorted_counts = sorted(counts.values())
+    sorted_counts.reverse()
+    hand_value = sorted_counts[0] * 10
+    if len(sorted_counts) > 1:
+        hand_value += sorted_counts[1]
+    key += 10 ** 10 * hand_value
+    return key
+
+hands_and_bids = []
+with open("input.txt") as infile:
+    for line in infile.read().splitlines():
+        hand, bid = line.split()
+        hands_and_bids.append((hand, int(bid)))
+hands_and_bids.sort(key=hand_key)
+
+winnings = 0
+for i in range(len(hands_and_bids)):
+    hand_winnings = hands_and_bids[i][1] * (i + 1)
+    winnings += hand_winnings
+    print(f"{i+1} ({hands_and_bids[i][0]}): {hands_and_bids} -> {hand_winnings}")
+print(f"{winnings=}")
